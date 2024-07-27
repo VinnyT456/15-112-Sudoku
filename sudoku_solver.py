9d14c8150d702e33b543027584844d8c4c77b7a1
Vinny456 <vincent03280608@gmail.com>
2024-07-27 16:03:08 -0400

    Organized Code with new a class

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,20 +1,58 @@
 import time
 import os
-class sudokuSolver:
+
+class sudokuBoard:
     def __init__(self,board):
         self.board = board
+
+    def getBoardValue(self,row,col):
+        return self.board[row][col]
+
+    def updateBoardValue(self,row,col,val):
+        self.board[row][col] = val
+
+    def clearBoardValue(self,row,col):
+        self.board[row][col] = 0
+
+    def checkRow(self,row,num):
+        for val in self.board[row]:
+            if val == num:
+                return False
+        return True
+    
+    def checkCol(self,col,num):
+        for current_row in self.board:
+            if current_row[col] == num:
+                return False
+        return True
+        
+    def checkBlock(self,row,col,num):
+        start_row = 3 * (row // 3)
+        start_col = 3 * (col // 3)
+        for current_row in range(start_row, start_row + 3):
+            for current_col in range(start_col, start_col + 3):
+                if self.board[current_row][current_col] == num:
+                    return False
+        return True
+    
+    #Check if the number can be placed in the current row and col
+    def checkLegalCell(self,row,col,num):
+        return self.checkRow(row,num) and self.checkCol(col,num) and self.checkBlock(row,col,num)
+    
+class sudokuSolver:
+    def __init__(self,board):
+        self.puzzle = board
         self.legal_numbers = {}
         self.positions_available = []
         self.initializeLegalNumbers()
 
-    #Create the original dictionary of all possible numbers
     def initializeLegalNumbers(self):
         #Assume the board is completely empty board every cell can be between 1 and 9
         initial_values = set(range(1,10))
         self.legal_numbers = dict()
         for row in range(9):
             for col in range(9):
-                if (self.board[row][col] == 0):
+                if (self.puzzle.getBoardValue(row,col) == 0):
                     self.legal_numbers[(row,col)] = initial_values
         self.updateLegalValues()
 
@@ -60,85 +98,55 @@ class sudokuSolver:
     #Update the legal moves dict to have the new values
     def updateLegalValues(self):
         for i in range(9):
-            row_seen = self.checkRow(i)
-            col_seen = self.checkCol(i)
+            row = col = i
+            block_row,block_col = i // 3 * 3,i % 3 * 3
+            row_seen = self.checkRow(row)
+            col_seen = self.checkCol(col)
+            block_seen = self.checkBlock(block_row,block_col)
             self.removeIllegalNumbersRow(i,row_seen)
             self.removeIllegalNumbersCol(i,col_seen)
-        for row in range(0,9,3):
-            for col in range(0,9,3):
-                block_seen = self.checkBlock(row,col)
-                self.removeIllegalNumbersBlock(row,col,block_seen)
+            self.removeIllegalNumbersBlock(block_row,block_col,block_seen)
         self.sortLegalValues()
-        
                 
     #See if the num in a specific row
-    def checkRow(self,row,num=-1):
-        if num != -1:
-            for val in self.board[row]:
-                if val == num:
-                    return False
-            return True
-        else:
-            seen = {val for val in self.board[row] if val != 0}
-            return seen
+    def checkRow(self,row):
+        return {val for val in self.puzzle.board[row] if val != 0}
 
     #See if the num in a specific col
-    def checkCol(self,col,num=-1):
-        if (num != -1):
-            for current_row in self.board:
-                if current_row[col] == num:
-                    return False
-            return True
-        else:
-            seen = {row[col] for row in self.board if row[col] != 0}
-            return seen
+    def checkCol(self,col):
+        return {row[col] for row in self.puzzle.board if row[col] != 0}
 
     #See if the num in a specific box
-    def checkBlock(self,row,col,num=-1):
+    def checkBlock(self,row,col):
         start_row = 3 * (row // 3)
         start_col = 3 * (col // 3)
-        if (num != -1):
-            for current_row in range(start_row, start_row + 3):
-                for current_col in range(start_col, start_col + 3):
-                    if self.board[current_row][current_col] == num:
-                        return False
-            return True
-        else:
-            seen = set()
-            for current_row in range(start_row, start_row + 3):
-                for current_col in range(start_col, start_col + 3):
-                    seen.add(self.board[current_row][current_col])
-            if (0 in seen):
-                seen.remove(0)
-            return seen
-
-    #Check if the number can be placed in the current row and col
-    def checkLegalCell(self,row,col,num):
-        return self.checkRow(row,num) and self.checkCol(col,num) and self.checkBlock(row,col,num)
-
-    #Find the first empty cell
-    def findEmptyCell(self):
-        if (self.positions_available != []):
-            return self.positions_available.pop(0)
-        return -1,-1
+        seen = set()
+        for current_row in range(start_row, start_row + 3):
+            for current_col in range(start_col, start_col + 3):
+                seen.add(self.puzzle.board[current_row][current_col])
+        if (0 in seen): 
+            seen.remove(0)
+        return seen
 
     def solveSudoku(self):
         return self.solveSudokuHelper()
 
     def solveSudokuHelper(self):
-        row,col = self.findEmptyCell()
+        #Gets the cell to solve
+        row,col = self.positions_available.pop(0) if self.positions_available != [] else (-1,-1)
         if row == -1 and col == -1:
-            return self.board
+            return self.puzzle.board
         else:
+            #Gets the possible values
             legal_value = self.legal_numbers[(row,col)]
             for val in legal_value:
-                if self.checkLegalCell(row,col,val):
-                    self.board[row][col] = val
+                if self.puzzle.checkLegalCell(row,col,val):
+                    self.puzzle.updateBoardValue(row,col,val)
                     self.initializeLegalNumbers()
                     board_solved = self.solveSudokuHelper()
                     if board_solved != None:
-                        return self.board
-                    self.board[row][col] = 0
+                        return board_solved
+                    self.puzzle.clearBoardValue(row,col)
             #Ensures it doesn't skip a cell after failing
             self.positions_available.insert(0,(row,col))
             return None
@@ -147,28 +155,26 @@ def readFile(path):
     with open(path, "rt") as f:
         return f.read()
     
-boards = {}
-    
-for filename in os.listdir('boards'):
-    if filename.endswith('.txt'):
-        pathToFile = f'boards/{filename}'
-        fileContents = readFile(pathToFile)
-        current_board = []
-        for row in fileContents.splitlines():
-            board = []
-            for i in row.split(' '):
-                board.append(int(i))
-            current_board.append(board)
-        boards[filename] = current_board
+#if __name__ == '__main__': copied from https://www.geeksforgeeks.org/what-does-the-if-__name__-__main__-do/
+if __name__ == '__main__':
+    boards = []
+        
+    for filename in os.listdir('boards'):
+        if filename.endswith('.txt'):
+            pathToFile = f'boards/{filename}'
+            fileContents = readFile(pathToFile)
+            current_board = []
+            for row in fileContents.splitlines():
+                board = []
+                for i in row.split(' '):
+                    board.append(int(i))
+                current_board.append(board)
+            boards.append(current_board)
 
-for name,board in boards.items():
-    start = time.time()
-    solver = sudokuSolver(board)
-    solved_board = solver.solveSudoku()
-    end = time.time()
-    print(f"Board {name} Time Spent {end-start}")
-
-#for key in solver.legal_numbers:
-#    print(f"{key} | {solver.legal_numbers[key]} | {solved_board[key[0]][key[1]]}")
-#for row in solved_board:
-#    print(row)
+    for board in boards:
+        start = time.time()
+        sudoku_board = sudokuBoard(board)
+        solver = sudokuSolver(sudoku_board)
+        solved_board = solver.solveSudoku()
+        end = time.time()
+        print(end-start)
