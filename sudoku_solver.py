4341c5065342f868dcde4afa2d24551ae966dbc8
Vinny456 <vincent03280608@gmail.com>
2024-07-27 13:00:42 -0400

    Fixed bug with the solving function

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,15 +1,17 @@
+import time
+import os
 class sudokuSolver:
     def __init__(self,board):
         self.board = board
         self.legal_numbers = {}
         self.positions_available = []
-        self.numbers_available = [] 
         self.initializeLegalNumbers()
 
     #Create the original dictionary of all possible numbers
     def initializeLegalNumbers(self):
         #Assume the board is completely empty board every cell can be between 1 and 9
         initial_values = set(range(1,10))
+        self.legal_numbers = dict()
         for row in range(9):
             for col in range(9):
                 if (self.board[row][col] == 0):
@@ -44,16 +46,16 @@ class sudokuSolver:
     #Learned (sorted(key)) from https://www.geeksforgeeks.org/python-sorted-function/
     #Learn items (dictionary) from https://www.w3schools.com/python/ref_dictionary_items.asp
     def sortLegalValues(self):
-        self.numbers_available = sorted([self.legal_numbers[position] for position in self.legal_numbers],key=len)
+        numbers_available = sorted([self.legal_numbers[position] for position in self.legal_numbers],key=len)
         self.positions_available = []
-        for number in self.numbers_available:
+        for number in numbers_available:
             for position,num in self.legal_numbers.items():
                 if (number == num and position not in self.positions_available):
                     self.positions_available.append(position)
                     break
         self.legal_numbers = dict()
-        for i in range(len(self.numbers_available)):
-            self.legal_numbers[self.positions_available[i]] = self.numbers_available[i]
+        for i in range(len(numbers_available)):
+            self.legal_numbers[self.positions_available[i]] = numbers_available[i]
 
     #Update the legal moves dict to have the new values
     def updateLegalValues(self):
@@ -106,7 +108,8 @@ class sudokuSolver:
             for current_row in range(start_row, start_row + 3):
                 for current_col in range(start_col, start_col + 3):
                     seen.add(self.board[current_row][current_col])
-            seen.remove(0)
+            if (0 in seen):
+                seen.remove(0)
             return seen
 
     #Check if the number can be placed in the current row and col
@@ -115,62 +118,57 @@ class sudokuSolver:
 
     #Find the first empty cell
     def findEmptyCell(self):
-        '''if (self.position_available != []): 
-            row,col = self.positions_available.pop()
-            return row,col,self.legal_numbers[(row,col)]'''
-        for row in range(9):
-            for col in range(9):
-                if (self.board[row][col] == 0):
-                    return (row,col)
+        if (self.positions_available != []):
+            return self.positions_available.pop(0)
         return -1,-1
-        
+
     def solveSudoku(self):
         return self.solveSudokuHelper()
-    
-    #Solve all the cells with only 1 legal value first
-    def quickSolve(self):
-        while self.positions_available[0] == 1:
-            #Check if the first position only haves 1 possible value
-            #Assuming it's all sorted lowest to highest
-            if (len(self.numbers_available[0]) == 1):
-                num = self.numbers_available.pop(0)
-                row,col = self.positions_available.pop(0)
-                self.legal_numbers.pop((row,col))
-                self.board[row][col] = num.pop()
-            #If the next one in line isn't a single number update it
-            if (len(self.numbers_available[0]) != 1):
-                self.initializeLegalNumbers
-                self.legal_numbers = dict()
-                self.updateLegalValues()
 
     def solveSudokuHelper(self):
         row,col = self.findEmptyCell()
         if row == -1 and col == -1:
             return self.board
         else:
-            for i in range(1,10):
-                if self.checkLegalCell(row,col,i):
-                    self.board[row][col] = i
+            legal_value = self.legal_numbers[(row,col)]
+            for val in legal_value:
+                if self.checkLegalCell(row,col,val):
+                    self.board[row][col] = val
+                    self.initializeLegalNumbers()
                     board_solved = self.solveSudokuHelper()
-                    if board_solved is not None:
+                    if board_solved != None:
                         return self.board
                     self.board[row][col] = 0
+            #Ensures it doesn't skip a cell after failing
+            self.positions_available.insert(0,(row,col))
             return None
         
-board = [
-    [7, 8, 0, 4, 0, 0, 1, 2, 0],
-    [6, 0, 0, 0, 7, 5, 0, 0, 9],
-    [0, 0, 0, 6, 0, 1, 0, 7, 8],
-    [0, 0, 7, 0, 4, 0, 2, 6, 0],
-    [0, 0, 1, 0, 5, 0, 9, 3, 0],
-    [9, 0, 4, 0, 6, 0, 0, 0, 5],
-    [0, 7, 0, 3, 0, 0, 0, 1, 2],
-    [1, 2, 0, 0, 0, 7, 4, 0, 0],
-    [0, 4, 9, 2, 0, 6, 0, 0, 7]
-]
-solver = sudokuSolver(board)
-solved_board = solver.solveSudoku()
-#for key,value in list(solver.legal_numbers.items()):
-#    print(f"{key} | {value} | {solved_board[key[0]][key[1]]}")
+def readFile(path):
+    with open(path, "rt") as f:
+        return f.read()
+    
+boards = {}
+    
+for filename in os.listdir('boards'):
+    if filename.endswith('.txt'):
+        pathToFile = f'boards/{filename}'
+        fileContents = readFile(pathToFile)
+        current_board = []
+        for row in fileContents.splitlines():
+            board = []
+            for i in row.split(' '):
+                board.append(int(i))
+            current_board.append(board)
+        boards[filename] = current_board
+
+for name,board in boards.items():
+    start = time.time()
+    solver = sudokuSolver(board)
+    solved_board = solver.solveSudoku()
+    end = time.time()
+    print(f"Board {name} Time Spent {end-start}")
+
+#for key in solver.legal_numbers:
+#    print(f"{key} | {solver.legal_numbers[key]} | {solved_board[key[0]][key[1]]}")
 #for row in solved_board:
 #    print(row)
