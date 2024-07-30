e76ecc0e0d2d169296933707a9a6152bed208358
Vinny456 <vincent03280608@gmail.com>
2024-07-30 16:19:53 -0400

    added new images

--- help_screen.py
+++ help_screen.py
@@ -1,8 +1,6 @@
 from cmu_graphics import *
 from PIL import Image
 
-import os
-
 class Buttons:
     def __init__(self,name,image,x_coord,y_coord,width,height):
         self.name = name
@@ -16,26 +14,19 @@ class Buttons:
         drawRect(self.x_coord+5,self.y_coord,self.width,self.height,fill=None)
         drawImage(self.image,self.x_coord,self.y_coord)
 
-    def checkButtonClick(self,app,x,y):
-        left = self.x_coord
-        right = self.x_coord+self.width
-        top = self.y_coord
-        bottom = self.y_coord+self.height
-        if (left <= x <= right and top <= y <= bottom):
-            self.buttonAction(app)
-
 def initializeImageBoard(app):
     positions = {
         'Instructions':(195,5,0.1,0.1),
         'Enter':(5,100,0.1,0.1),
-        'Back':(5,195,0.1,0.1),
-        'Hint':(425,195,0.1,0.1),
-        'New':(5,290,0.1,0.1),
-        'Apply':(425,290,0.1,0.1),
-        'Undo':(5,385,0.1,0.1),
-        'Automatic':(425,385,0.1,0.1),
-        'Manual':(5,490,0.1,0.1),
-        'Edit':(425,490,0.1,0.1),
+        'Game':(5,180,0.1,0.1),
+        'Back':(5,270,0.1,0.1),
+        'Hint':(425,270,0.1,0.1),
+        'New':(5,350,0.1,0.1),
+        'Apply':(425,350,0.1,0.1),
+        'Undo':(5,430,0.1,0.1),
+        'Automatic':(425,430,0.1,0.1),
+        'Manual':(5,510,0.1,0.1),
+        'Edit':(425,510,0.1,0.1),
     }
     for name in positions:
         image = (Image.open(f"button_image/{name}.png"))
@@ -47,10 +38,6 @@ def initializeImageBoard(app):
 def helpScreen_onAppStart(app):
     app.help_images = []
     initializeImageBoard(app)
-
-'''def helpScreen_onMousePress(app,mouseX,mouseY):
-    app.main_images[-1].checkButtonClick(mouseX,mouseY)
-    app.main_images[-2].checkButtonClick(mouseX,mouseY)'''
     
 def helpScreen_onKeyPress(app,key):
     if (key == 'enter'):
